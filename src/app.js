8c77a1db9d5526d03f8354c2c979cd817749365b
melissakipp <melissakipp.az@gmail.com>
2021-06-06 09:43:26 -0700

    Add Posts & Show Alert

--- src/app.js
+++ src/app.js
@@ -6,17 +6,35 @@ import { ui } from './ui';
 // Get posts on DOM load
 document.addEventListener('DOMContentLoaded', getPosts);
 
+// Listen for add post
+document.querySelector('.post-submit').addEventListener('click', submitPost);
+
+// Get Posts
 function getPosts() {
   http.get('http://localhost:3000/posts')
     .then(data => ui.showPosts(data))
     .catch(err => console.log(err));
 }
 
+// Submit Post
+function submitPost() {
+  const title = document.querySelector('#title').value;
+  const body = document.querySelector('#body').value;
 
+  const data = {
+    title,
+    body
+  }
 
-
-
-
+  // Create Post
+  http.post('http://localhost:3000/posts', data)
+    .then(data => {
+      ui.showAlert('Post added', 'alert alert-success');
+      ui.clearFields();
+      getPosts();
+    })
+    .catch(err => console.log(err));
+}
 
 // const getData = async (url) => {
 //   const response = await fetch(url);
@@ -25,9 +43,6 @@ function getPosts() {
 // };
 
 // getData('https://jsonplaceholder.typicode.com/posts');
-
-
-
 
 
 // Bret Fisher: Docker for Node.js course Video 25
